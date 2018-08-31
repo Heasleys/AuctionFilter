ab0ef82d3155f4718bef9ca41d6bb6534f57ca6c
Heasleys4hemp <heasleys4hemp@hotmail.com>
2018-08-31 13:22:19 -0400

    Update AuctionFilter.user.js

--- AuctionFilter.user.js
+++ AuctionFilter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auction Filter
 // @namespace    Heasleys4hemp
-// @version      0.1.1
+// @version      0.2
 // @description  Filter Items from the Torn Auction
 // @author       Heasleys4hemp [1468764]
 // @match        https://www.torn.com/amarket.php*
@@ -9,7 +9,7 @@
 // ==/UserScript==
 
 //array list of items to hide
-var arrList = ['Cricket Bat', 'Psycho Clown Mask', 'Twin Tiger Hooks', 'Old Lady Mask', 'Scarred Man Mask', 'Nun Mask', 'Young Lady Mask', 'Ginger Kid Mask', 'Moustache Man Mask', 'Kama', 'Exotic Gentleman Mask', 'Wushu Double Axes'];
+var arrList = ['Cricket Bat', 'Psycho Clown Mask', 'Twin Tiger Hooks', 'Old Lady Mask', 'Scarred Man Mask', 'Nun Mask', 'Young Lady Mask', 'Ginger Kid Mask', 'Moustache Man Mask', 'Kama', 'Exotic Gentleman Mask', 'Wushu Double Axes', 'Mini Cooper S'];
 //MutationObserver used to monitor specific activity
 const observer = new MutationObserver(() => {
     //observing item list div for li's that contain 'Cricket Bat' (now list of items)
