5a295efbd2ce93f52e4ad3a07495c940fbf18193
Heasley <heasleys4hemp@hotmail.com>
2019-12-09 04:22:55 -0500

    Update AuctionFilter.user.js

--- AuctionFilter.user.js
+++ AuctionFilter.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Auction Filter
-// @namespace    Heasleys4hemp.AuctionFilter
-// @version      0.3
+// @namespace    Heasleys.AuctionFilter
+// @version      0.3.1
 // @description  Filter Items from the Torn Auction
 // @author       Heasleys4hemp [1468764]
 // @match        https://www.torn.com/amarket.php*
-// @updateURL    https://github.com/Heasleys4hemp/AuctionFilter/raw/master/AuctionFilter.user.js
+// @updateURL    https://github.com/Heasleys/AuctionFilter/raw/master/AuctionFilter.user.js
 // ==/UserScript==
 
 //copy pagination-wrap (the page selector element) and paste it to the top of it's own parent element
@@ -16,7 +16,7 @@ $('.pagination-wrap').each(function(i, obj) {
 
 //array list of items to hide
 //I plan on making this better in the future, but it works for what I need it for.
-var arrList = ['Cricket Bat', 'Psycho Clown Mask', 'Twin Tiger Hooks', 'Old Lady Mask', 'Scarred Man Mask', 'Nun Mask', 'Young Lady Mask', 'Ginger Kid Mask', 'Moustache Man Mask', 'Kama', 'Exotic Gentleman Mask', 'Wushu Double Axes', 'Mini Cooper S'];
+var arrList = ['Guandao','Salt Shaker','Cricket Bat', 'Psycho Clown Mask', 'Twin Tiger Hooks', 'Old Lady Mask', 'Scarred Man Mask', 'Nun Mask', 'Young Lady Mask', 'Ginger Kid Mask', 'Moustache Man Mask', 'Kama', 'Exotic Gentleman Mask', 'Wushu Double Axes', 'Mini Cooper S'];
 //MutationObserver used to monitor specific activity
 const observer = new MutationObserver(() => {
     //observing item list div for li's that contain 'Cricket Bat' (now list of items)
